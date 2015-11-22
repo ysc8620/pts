5d9ff6774169479255c59584ab2ac60f0e663610
ysc8620 <ysc8620@163.com>
2015-11-22 16:01:47 +0800

    ds

---
+++ data/config.php
@@ -0,0 +1,22 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+// database name
+$db_name   = "pts1112";
+// database username
+$db_user   = "root";
+// database password
+$db_pass   = "LEsc2008";
+// table prefix
+$prefix    = "hhs_";
+$timezone    = "PRC";
+$cookie_path    = "/";
+$cookie_domain    = "";
+$session = "3600";
+
+define('EC_CHARSET','utf-8');
+define('ADMIN_PATH','sysadm');
+define('AUTH_KEY', 'this is a key');
+define('OLD_AUTH_KEY', '');
+define('API_TIME', '2014-12-05 11:15:28');
+?>
